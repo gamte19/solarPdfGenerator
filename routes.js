b4274e023ab5f60a1a44d9952933e174f5793418
gamte19 <gamte19@gmail.com>
2017-07-17 13:41:57 +0200

    OK

---
+++ routes.js
@@ -0,0 +1,46 @@
+var pdf = require('html-pdf');
+var appmodule = require('./server');
+var nunjucks = require("nunjucks");
+var path = require("path");
+var templateRoute = path.resolve(__dirname, 'templates/offer_template.html');
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+config = {
+    "format": "Letter",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+    "orientation": "portrait",
+    // Rendering options
+    "base": "file://localhost:1350/static" // Base path that's used to load files (images, css, js) when they aren't referenced using a host
+    };
+
+exports.printpdf1 = function (req, res) {
+    var offer = req.body;
+
+
+    var renderedHtml =  appmodule.env.render(templateRoute, {
+        numberWithCommas: numberWithCommas,
+        offer: offer
+    });
+    // This one automatically downloads the pdf
+    // pdf.create(renderedHtml).toBuffer(function(err, buffer){
+    //     console.log('This is a buffer:', Buffer.isBuffer(buffer));
+    //     res.download(buffer);
+    //
+    // });
+
+    // This one returns the pdf in your browser
+    // pdf.create(renderedHtml).toStream(function(err, stream){
+    //     console.log(stream);
+    //     stream.pipe(res);
+    // });
+
+    // This one saves it into a file
+    pdf.create(renderedHtml, config).toFile('./offer_' + offer.id + '.pdf', function(err, res) {
+        if (err) return console.log(err);
+        else console.log("Your PDF has been generated!");
+    });
+    res.json("ok");
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var http = require('http');
+var path = require ('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var pdf = require('html-pdf');
+var nunjucks = require('nunjucks');
+var html = fs.readFileSync('./templates/offer_template.html', 'utf8');
+
+
+
+var routes = require('./routes');
+var app = express();
+
+//Nunjucks is a product from Mozilla and we are using it as a template engine.
+exports.env = nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+});
+
+var port = process.env.port || 1350;
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+
+
+
+// Add headers for CORS
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.post('/api/printpdf1', routes.printpdf1);
+
+app.listen(port);
+console.log('node server for html-pdf is running on port ' + port);
